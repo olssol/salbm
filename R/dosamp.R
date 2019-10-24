353583f05dba7a56964d583084f7b4db35fcbfbe
cwang68 <cwang68@CGs-MacBook-Pro.local>
2019-10-23 23:25:31 -0400

    add print(bootstrap no)

--- R/dosamp.R
+++ R/dosamp.R
@@ -1,15 +1,16 @@
 # dosamp: read a sample from samples based on the sampno column, tilt by alphas
 dosamp <- function( sampno, samples,  rf.ntree = 1000, rf.seed = -1, rf.sampsize, rf.nodesize = 1, alphas = c(0) )  {
+   print(sampno)
    ncolsamp               <-   ncol(samples) - 1
    sample                 <-   samples[ samples[,1] == sampno, 2:(ncolsamp+1) ]
-      
+
    sample                 <-   as.data.frame(sample)
    colnms                 <-   paste("y",1:ncolsamp,sep="")
    names(sample)          <-   colnms
    sample[]               <-   lapply(sample,function(x) factor(x,levels=c("0","1","2")))
 
    # random forest joint prob distribution
-   jp                     <-   rfjp( data = sample, ntree = rf.ntree, seed = rf.seed, sampsize = rf.sampsize, nodesize = rf.nodesize )
+   jp                     <-   rfjp(data = sample, ntree = rf.ntree, seed = rf.seed, sampsize = rf.sampsize, nodesize = rf.nodesize )
    tiltResults            <-   lapply( alphas, function(x) tilt(x,jp) )
    tiltResults            <-   do.call(rbind,tiltResults)
    tiltResults            <-   as.data.frame(tiltResults)
--- R/salbmq.R
+++ R/salbmq.R
@@ -1,9 +1,9 @@
 salbmq <- function( j, alpha, main, bs ) {
-   e1   <- main[ main[,"alpha"] == alpha, j] 
+   e1   <- main[ main[,"alpha"] == alpha, j]
    vals <- bs[ bs[,"alpha"] == alpha, j ]
-   q1   <- quantile(vals,0.025)
-   q2   <- quantile(vals,0.975)
-   q3   <- quantile(abs(vals-e1), 0.95)
+   q1   <- quantile(vals,0.025, na.rm = TRUE)
+   q2   <- quantile(vals,0.975, na.rm = TRUE)
+   q3   <- quantile(abs(vals-e1), 0.95, na.rm = TRUE)
    outv <- c( alpha, j, e1, q1, q2, e1-q3, e1+q3 )
    return(outv)
 }
